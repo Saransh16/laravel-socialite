f19b130221a7a1592e7a702967b1c104801e5a5b
Saransh Kumar <saranshkumar@Saransh-MBP.local>
2021-10-19 03:22:29 +0530

    Add in sign in form and github credentials

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/auth/google/redirect', function () {
+    return Socialite::driver('google')->redirectUrl('http://localhost:8000/auth/google/redirect')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $user = Socialite::driver('google')->stateless()->user();
+    dd($user);
+});
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/github/callback', function () {
+    $user = Socialite::driver('github')->stateless()->user();
+    dd($user);
+});
 
 Route::get('/', function () {
     return view('welcome');
