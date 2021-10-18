ea09270f161e99dd4c5173bfd1d243c0ab6c554d
Saransh Kumar <saranshkumar@Saransh-MBP.local>
2021-10-19 02:14:40 +0530

    Make changes to the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'provider',
+        'provide_id'
     ];
 
     /**
@@ -30,15 +31,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
